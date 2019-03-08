b6836343e653a9848177dab896b30fe2dee6227b
David <david.martin.carl@gmail.com>
2019-03-08 14:31:21 +0100

    close #6 ApiKey out as an authentication

---
+++ test/recipe.js
@@ -0,0 +1,15 @@
+require('should');
+
+const zapier = require('zapier-platform-core');
+
+const App = require('../index');
+const appTester = zapier.createAppTester(App);
+
+describe('creates', () => {
+    describe('new recipe trigger', () => {
+        it('should load recipes', (done) => {
+            done();
+        });
+    });
+
+});
